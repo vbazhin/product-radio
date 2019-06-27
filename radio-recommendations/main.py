61d39f8d7cdcb30f7616ce675bb59d9d40f4c2a5
vbazhin <v.bazhin@gmail.com>
2019-06-27 18:57:22 +1000

    Add basic recommendations template

--- radio-recommendations/main.py
+++ radio-recommendations/main.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
+
 
 @app.route('/<int:radio_product_id>')
 def show_products(radio_product_id):
-    return 'Showing products for {}'.format(radio_product_id)
+    # Mock data. Request the API here.
+    product_data = {
+        'product_id': 'product id',
+        'product_name': 'Mock product_name',
+        'image_url': 'http://flask.pocoo.org/docs/1.0/_static/flask.png',
+        'description': 'Mock description'
+    }
+    products = [product_data]*5
+    # return 'Showing products for {}'.format(radio_product_id)
+    return render_template(
+        'base.html',
+        products=products,
+        radio_id=radio_product_id
+   )
 
 if __name__ == '__main__':
    app.run(debug=True)
